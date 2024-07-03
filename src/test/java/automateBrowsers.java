aa4d85b3b546f7d2caf9714dc35cdb3661fa5d22
Büşra <dytbusrasezen@gmail.com>
2024-07-03 12:55:56 +0300

    initial commit

---
+++ src/test/java/automateBrowsers.java
@@ -0,0 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class automateBrowsers {
+    public static void main(String[] args) {
+       // WebDriverManager.firefoxdriver().setup();
+       // WebDriver driver = new FirefoxDriver();
+        // WebDriverManager.edgedriver().setup();
+        // WebDriver driver = new EdgeDriver();
+        WebDriverManager.safaridriver().setup();
+        WebDriver driver = new SafariDriver();
+    }
+}
---
+++ src/test/java/myFirstClass.java
@@ -0,0 +1,41 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class myFirstClass {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get( "https://www.google.com" );
+
+        // String googleSource = driver.getPageSource();
+        //String googleHandle = driver.getWindowHandle();
+        //System.out.println( googleHandle );
+
+        String googleTitle = driver.getTitle();
+        System.out.println( "Google title is:" +googleTitle);
+
+        driver.navigate().to("https://www.udemy.com" );
+
+        String udemyTitle = driver.getTitle();
+        System.out.println( "Udemy title is:" +udemyTitle);
+
+        String udemyUrl = driver.getCurrentUrl();
+        System.out.println("Udemy url is:" + udemyUrl);
+        driver.navigate().back();
+        String googleUrl = driver.getCurrentUrl();
+        System.out.println("Google url is:" + googleUrl);
+
+        driver.manage().window().fullscreen();
+        driver.navigate().refresh();
+        googleUrl = driver.getCurrentUrl();
+        System.out.println("Google url is:" + googleUrl);
+
+        driver.navigate().forward();
+        udemyUrl = driver.getCurrentUrl();
+        System.out.println("Udemy url is:" + udemyUrl);
+        
+        driver.quit();
+    }
+}
