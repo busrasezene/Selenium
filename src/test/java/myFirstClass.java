b464fc59ac11ae42171cd8c9324b6b990e9609d0
Büşra <124506690+busrasezene@users.noreply.github.com>
2024-07-11 11:30:31 +0300

    Update myFirstClass.java

--- src/test/java/myFirstClass.java
+++ src/test/java/myFirstClass.java
@@ -44,7 +44,17 @@ public class myFirstClass {
     }
 }                              
 
-WebDriverManager.chromedriver().setup();
+//Geçerli bilgiler girilerek başarılı Login olmak;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class myFirstClass {
+    public static void main(String[] args) { 
+        
+
+        WebDriverManager.chromedriver().setup();
         //Kurulum
         WebDriver driver = new ChromeDriver();
         //Web sitesini açmak için
@@ -59,3 +69,6 @@ WebDriverManager.chromedriver().setup();
         //oturum açma fonksiyonunu doğrulamak (Login butonu)
         driver.quit();
         //Tarayıcıyı kapatır
+
+    }
+}   
